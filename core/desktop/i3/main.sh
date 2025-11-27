f769b44f646e70fcbf833ecc12887ec2329a9e42
neo <neo@neo.neo>
2025-11-27 09:06:41 -0500

    i3 upload

---
+++ core/desktop/i3/main.sh
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt install -y \
+i3-wm i3blocks rofi xdotool dex \
+acpi upower xfce4-power-manager policykit-1 \
+i3lock xss-lock \
+pulseaudio-utils brightnessctl \
+feh picom \
+fonts-font-awesome
+

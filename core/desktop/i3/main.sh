1909d8c5a324c21e104af747a5e0c68b20c1324e
Abr-ahamis <87413272+Abr-ahamis@users.noreply.github.com>
2025-11-29 15:45:53 -0500

    Delete core/desktop directory

--- core/desktop/i3/main.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update && sudo apt install -y \
-i3-wm i3blocks rofi xdotool dex \
-acpi upower xfce4-power-manager policykit-1 \
-i3lock xss-lock \
-pulseaudio-utils brightnessctl \
-feh picom \
-fonts-font-awesome
-
